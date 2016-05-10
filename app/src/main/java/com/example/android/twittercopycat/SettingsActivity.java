98c5cebb0215972b3c68553fe9e79ea637c0af99
Isabel Costa <isabelcmdcosta@gmail.com>
2016-05-10 17:24:47 +0100

    notifications in progress

--- app/src/main/java/com/example/android/twittercopycat/SettingsActivity.java
+++ app/src/main/java/com/example/android/twittercopycat/SettingsActivity.java
@@ -1,6 +1,9 @@
 package com.example.android.twittercopycat;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 
@@ -25,6 +28,15 @@ public class SettingsActivity extends PreferenceActivity {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.preferences);
 
+        }
+    }
+
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        Preference pref = findPreference(key);
+
+        if (pref instanceof ListPreference) {
+            ListPreference listPref = (ListPreference) pref;
+            pref.setSummary(listPref.getEntry());
         }
     }
 }
--- app/src/main/java/com/example/android/twittercopycat/TwitterHelper.java
+++ app/src/main/java/com/example/android/twittercopycat/TwitterHelper.java
@@ -1,5 +1,10 @@
 package com.example.android.twittercopycat;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import java.util.List;
@@ -31,6 +36,52 @@ public class TwitterHelper {
         return true;
     }
 
+    public void showNotification(String tweet, Context ctx) {
 
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(ctx)
+                        .setSmallIcon(R.drawable.notification_icon)
+                        .setContentTitle("New Tweet!")
+                        .setContentText(tweet);
+
+        Intent resultIntent = new Intent(ctx, TimelineFragment.class);
+//        ...
+        // Because clicking the notification opens a new ("special") activity, there's
+        // no need to create an artificial back stack.
+        PendingIntent resultPendingIntent =
+                PendingIntent.getActivity(
+                        ctx,
+                        0,
+                        resultIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+
+        mBuilder.setContentIntent(resultPendingIntent);
+
+//        ...
+        // Sets an ID for the notification
+        int mNotificationId = 001;
+        // Gets an instance of the NotificationManager service
+        NotificationManager mNotifyMgr =
+                (NotificationManager) ctx.getSystemService(ctx.NOTIFICATION_SERVICE);
+        // Builds the notification and issues it.
+        mNotifyMgr.notify(mNotificationId, mBuilder.build());
+//
+//        // Set the icon, scrolling text and timestamp
+//        Notification notification = new Notification(R.drawable.noti_icon,
+//                "Brand new tweet!", System.currentTimeMillis());
+//
+//        // The PendingIntent to launch our activity if the user selects this
+//        // notification
+//        PendingIntent contentIntent = PendingIntent.getActivity(ctx, 0,
+//                new Intent(ctx, MyTimelineScreen.class), 0);
+//
+//        // Set the info for the views that show in the notification panel.
+//        notification.setLatestEventInfo(ctx, "New Tweet? like i think", tweet,
+//                contentIntent);
+//
+//        // Send the notification.
+//        mNotifyMgr.notify("Timeline updated!", 0, notification);
+    }
 
 }
