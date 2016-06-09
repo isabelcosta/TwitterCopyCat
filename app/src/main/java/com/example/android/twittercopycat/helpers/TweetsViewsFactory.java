b84919677500ec34777d155b4de9c6049bf9d586
Isabel Costa <isabelcmdcosta@gmail.com>
2016-06-09 12:17:41 +0100

    last tweets in widget working fine | misses check for new tweets periodically

--- app/src/main/java/com/example/android/twittercopycat/helpers/TweetsViewsFactory.java
+++ app/src/main/java/com/example/android/twittercopycat/helpers/TweetsViewsFactory.java
@@ -9,24 +9,35 @@ import android.widget.RemoteViewsService;
 
 import com.example.android.twittercopycat.R;
 import com.example.android.twittercopycat.TCCWidgetProvider;
+import com.example.android.twittercopycat.application.TwitterCopyCatApplication;
+
+import java.util.List;
+
+import winterwell.jtwitter.Twitter;
 
 public class TweetsViewsFactory implements RemoteViewsService.RemoteViewsFactory {
-    private static final String[] items={"lorem", "ipsum", "dolor",
-            "sit", "amet", "consectetuer",
-            "adipiscing", "elit", "morbi",
-            "vel", "ligula", "vitae",
-            "arcu", "aliquet", "mollis",
-            "etiam", "vel", "erat",
-            "placerat", "ante",
-            "porttitor", "sodales",
-            "pellentesque", "augue",
-            "purus"};
+
+//    private static final String[] tweets={"lorem", "ipsum", "dolor",
+//            "sit", "amet", "consectetuer",
+//            "adipiscing", "elit", "morbi",
+//            "vel", "ligula", "vitae",
+//            "arcu", "aliquet", "mollis",
+//            "etiam", "vel", "erat",
+//            "placerat", "ante",
+//            "porttitor", "sodales",
+//            "pellentesque", "augue",
+//            "purus"};
+
+    private final static int WIDGET_NUMBER_OF_TWEETS = 5;
+    private TwitterCopyCatApplication app = TwitterCopyCatApplication.getInstance();
+    private String[] tweets = new String[WIDGET_NUMBER_OF_TWEETS];
+
     private Context ctxt=null;
     private int appWidgetId;
 
     public TweetsViewsFactory(Context ctxt, Intent intent) {
-        this.ctxt=ctxt;
-        appWidgetId=intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,
+        this.ctxt = ctxt;
+        appWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,
                 AppWidgetManager.INVALID_APPWIDGET_ID);
     }
 
@@ -42,24 +53,24 @@ public class TweetsViewsFactory implements RemoteViewsService.RemoteViewsFactory
 
     @Override
     public int getCount() {
-        return(items.length);
+        return(tweets.length);
     }
 
     @Override
     public RemoteViews getViewAt(int position) {
-        RemoteViews row=new RemoteViews(ctxt.getPackageName(),
+        RemoteViews row = new RemoteViews(ctxt.getPackageName(),
                 R.layout.tweet_widget_view);
 
-        row.setTextViewText(android.R.id.text1, items[position]);
+        row.setTextViewText(R.id.widget_list_text_view, tweets[position]);
 
-        Intent i=new Intent();
-        Bundle extras=new Bundle();
+        Intent intent = new Intent();
+        Bundle extras = new Bundle();
 
-        extras.putString(TCCWidgetProvider.EXTRA_WORD, items[position]);
-        i.putExtras(extras);
-        row.setOnClickFillInIntent(android.R.id.text1, i);
+        extras.putString(TCCWidgetProvider.EXTRA_WORD, tweets[position]);
+        intent.putExtras(extras);
+        row.setOnClickFillInIntent(R.id.widget_list_text_view, intent);
 
-        return(row);
+        return (row);
     }
 
     @Override
@@ -84,6 +95,25 @@ public class TweetsViewsFactory implements RemoteViewsService.RemoteViewsFactory
 
     @Override
     public void onDataSetChanged() {
-        // no-op
+
+        if(app.isNetworkAvailable()){
+            // TODO: 08-06-2016 put get public timeline in helper
+            //Check for public Timeline
+            Twitter t = new Twitter();
+            t.setAPIRootUrl(app.getApiUrl());
+            t.setCount(WIDGET_NUMBER_OF_TWEETS);
+
+            // Get the last 5 tweets
+            List<Twitter.Status> fetchedTweets = t.getPublicTimeline();
+
+            if(!fetchedTweets.isEmpty()){
+                for(int i=0; i < WIDGET_NUMBER_OF_TWEETS; i++){
+                    tweets[i] = fetchedTweets.get(i).getText();
+                }
+            }
+
+        } else {
+            //get tweets from database
+        }
     }
 }
--- app/src/main/java/com/example/android/twittercopycat/services/WidgetService.java
+++ app/src/main/java/com/example/android/twittercopycat/services/WidgetService.java
@@ -3,6 +3,7 @@ package com.example.android.twittercopycat.services;
 import android.content.Intent;
 import android.widget.RemoteViewsService;
 
+import com.example.android.twittercopycat.application.TwitterCopyCatApplication;
 import com.example.android.twittercopycat.helpers.TweetsViewsFactory;
 
 public class WidgetService extends RemoteViewsService {
@@ -12,41 +13,11 @@ public class WidgetService extends RemoteViewsService {
     * */
 
     private final static int WIDGET_NUMBER_OF_TWEETS = 5;
+    private TwitterCopyCatApplication app = TwitterCopyCatApplication.getInstance();
+    private String[] tweets = new String[WIDGET_NUMBER_OF_TWEETS];
 
     @Override
     public RemoteViewsFactory onGetViewFactory(Intent intent) {
-
-/*
-        int appWidgetId = intent.getIntExtra(
-                AppWidgetManager.EXTRA_APPWIDGET_ID,
-                AppWidgetManager.INVALID_APPWIDGET_ID
-        );
-
-        TwitterCopyCatApplication app = TwitterCopyCatApplication.getInstance();
-
-        String[] items={"lorem", "ipsum", "dolor",
-                "sit", "amet", "consectetuer",
-                "adipiscing", "elit", "morbi",
-                "vel", "ligula", "vitae",
-                "arcu", "aliquet", "mollis",
-                "etiam", "vel", "erat",
-                "placerat", "ante",
-                "porttitor", "sodales",
-                "pellentesque", "augue",
-                "purus"};
-
-        if(app.isNetworkAvailable()){
-            // TODO: 08-06-2016 put get public timeline in helper
-            //Check for public Timeline
-            Twitter t = new Twitter();
-            t.setAPIRootUrl(app.getApiUrl());
-            t.setCount(WIDGET_NUMBER_OF_TWEETS);
-
-            // Get the last 5 tweets
-            List<Twitter.Status> tweets = t.getPublicTimeline();
-        }
-*/
         return (new TweetsViewsFactory(this.getApplicationContext(), intent));
     }
-
 }
